69b2dda9e94f769f88e5994e62a4ff041305b16f
Matt F <fiaschettima@gmail.com>
2022-07-06 19:08:48 -0400

    prints one card to page from fetch results

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -28,11 +28,55 @@ function checkInput() {
             return response.json();
     }).then(function(data){
         console.log(data);
+        ///////////////////////////////////////////////////////////////
+        var newRow = document.createElement('div'); // make row
+        var rightSide = document.createElement('div'); // make col
+        var movieCard = document.createElement('div');// make card
+        var movieImgC = document.createElement('div');// image and title
+        var movieContent = document.createElement('div');// card content
+        var movieImg = document.createElement('img')// add image
+        var movieTitle = document.createElement('span')// floating title in img
+        var movieText = document.createElement('p')// text for movie
+        rightSide.classList.add('col','s12','m7');
+        newRow.classList.add('row')
+        movieCard.classList.add('card')
+        movieImgC.classList.add('card-image')
+        movieImg.setAttribute('src', 'http://image.tmdb.org/t/p/w500/'+data.results[0].backdrop_path)+'.jpg'
+        movieText.classList.add('card-content')
+        movieTitle.classList.add('card-title')
+        movieTitle.textContent = data.results[0].title
+        movieText.textContent =  data.results[0].overvie
+        movieContent.appendChild(movieText)
+        movieImgC.appendChild(movieImg)
+        movieImgC.appendChild(movieTitle)
+        movieCard.appendChild(movieImgC)
+        movieCard.appendChild(movieContent)
+        rightSide.appendChild(movieCard)
+        newRow.appendChild(rightSide)
+        console.log(newRow)
+        var pageBody = document.getElementById('cardHere')
+        pageBody.appendChild(newRow)
     })
 }
-
-
-  
+// https://api.themoviedb.org/3/movie/414906/similar?api_key=07f3bf91adb1325ab2741c977ecdf895&language=en-US&page=1
+/* <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+   */
 // findyoutubeid();
 //     console.log(userInput.value);
 // }
