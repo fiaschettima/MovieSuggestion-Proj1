76ee674e2759d234d8af8445f24eead865bb75e0
JZ <jeffz98@berkeley.edu>
2022-07-06 14:38:26 -0700

    added click event listener

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,25 @@
 
-function findyoutubeid(){
-var youtubeAPI = 'AIzaSyDEMdWu3lqGoSduYcL2p7LMJwCINR_eA0o'
-var youtubeSearchUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='+movieName+'&videoEmbeddable=videoEmbeddableUnspecified&key='+ youtubeAPI;
-fetch(youtubeSearchUrl)
-    .then(function(response){
-        console.log(response)
-        return response.json();
-}).then(function(data){
-    console.log(data);
-})
+// function findyoutubeid(){
+// var youtubeAPI = 'AIzaSyDEMdWu3lqGoSduYcL2p7LMJwCINR_eA0o'
+// var youtubeSearchUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='+movieName+'&videoEmbeddable=videoEmbeddableUnspecified&key='+ youtubeAPI;
+// fetch(youtubeSearchUrl)
+//     .then(function(response){
+//         console.log(response)
+//         return response.json();
+// }).then(function(data){
+//     console.log(data);
+// })
+// }
+// findyoutubeid();
+
+var userInput = document.getElementById("search-topic");
+var topicTitle = document.getElementById("title-name");
+var topicGenre = document.getElementById("genre-name");
+var submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener('click', checkInput);
+
+function checkInput() {
+    
+    console.log(userInput.value);
 }
-findyoutubeid();
+
