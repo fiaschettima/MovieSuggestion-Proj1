2809f7a512d9515fe4eb07f34435484672176023
Matt F <fiaschettima@gmail.com>
2022-07-08 18:48:32 -0400

    yt function now works per each button basedon the content of the card that contains that button

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,19 +6,7 @@ var topicTitle = document.getElementById("title-name");
 var topicGenre = document.getElementById("genre-name");
 var submitBtn = document.getElementById("submit-btn");
 submitBtn.addEventListener('click', checkInput);
-submitBtn.addEventListener('click', findyoutubeid);
-
-function findyoutubeid(){
-var youtubeAPI = 'AIzaSyDEMdWu3lqGoSduYcL2p7LMJwCINR_eA0o'
-var youtubeSearchUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='+this.dataset.ytsearch +'trailer&videoEmbeddable=videoEmbeddableUnspecified&key='+ youtubeAPI;
-fetch(youtubeSearchUrl)
-    .then(function(response){
-        return response.json();
-}).then(function(data){
-    console.log(data);
-    trailerVID.setAttribute('src', 'https://www.youtube.com/embed/'+ data.items[0].id.videoId)
-})
-}
+// submitBtn.addEventListener('click', findyoutubeid);
 
 function checkInput() {
     var tmdbAPI = '07f3bf91adb1325ab2741c977ecdf895';
@@ -59,57 +47,75 @@ function checkInput() {
       
         // pageBody.appendChild(rightSide)
 
-        var testCard = document.createElement('div')
-        var testCardImgCon = document.createElement('div')
-        var testImage = document.createElement('img')
-        var testContentCont = document.createElement('div')
-        var testSpan = document.createElement('span')
-        var testPar = document.createElement('p')
-        var testAnch = document.createElement('a')
-        var testDivreveal = document.createElement('div')
-        var testSpanRevTitle = document.createElement('span')
-        var closeRev = document.createElement('i')
-        var OpenRev = document.createElement('i')
-        var revPara = document.createElement('p')
-        testCard.classList.add('card','col','s12','m6', 'l4', 'xl3')
-        testCardImgCon.classList.add('card-image', 'waves-effect', 'waves-block', 'waves-light')
-        testImage.classList.add('activator')
-        testContentCont.classList.add('card-content')
-        testSpan.classList.add('card-title', 'activator','grey-text', 'text-darken-4')
-        testDivreveal.classList.add('card-reveal')
-        testSpanRevTitle.classList.add('card-title','grey-text', 'text-darken-4')
-        closeRev.classList.add('material-icons', 'right')
-        closeRev.textContent = 'close'
-        OpenRev.classList.add('material-icons', 'right')
-        OpenRev.textContent = 'more_vert'
-        testImage.setAttribute('src', 'http://image.tmdb.org/t/p/w342/'+data.results[i].poster_path)+'.jpg'
-        testSpan.textContent = data.results[i].title
-        testAnch.setAttribute('href', '#trailer')
-        testAnch.setAttribute('data-ytSearch', data.results[0].title)
-        testAnch.setAttribute('onclick', findyoutubeid)
-        testAnch.classList.add('waves-effect', 'waves-light', 'modal-trigger', 'findtrailer')
-        testAnch.textContent = 'Watch The Trailer'
-        testSpanRevTitle.textContent = data.results[i].title
-        revPara.textContent = data.results[i].overview
-        testCardImgCon.appendChild(testImage)
-        testSpan.appendChild(OpenRev)
-        testContentCont.appendChild(testSpan)
-        testPar.appendChild(testAnch)
-        testContentCont.appendChild(testPar)
-        testSpanRevTitle.appendChild(closeRev)
-        testDivreveal.appendChild(testSpanRevTitle)
-        testDivreveal.appendChild(revPara)
-        testCard.appendChild(testCardImgCon)
-        testCard.appendChild(testContentCont)
-        testCard.appendChild(testDivreveal)
-        pageBody.appendChild(testCard)
-        var trailerButton = document.getElementsByClassName('findtrailer')
+        var testCard = document.createElement('div');
+        var testCardImgCon = document.createElement('div');
+        var testImage = document.createElement('img');
+        var testContentCont = document.createElement('div');
+        var testSpan = document.createElement('span');
+        var testPar = document.createElement('p');
+        var testAnch = document.createElement('button');
+        var testDivreveal = document.createElement('div');
+        var testSpanRevTitle = document.createElement('span');
+        var closeRev = document.createElement('i');
+        var OpenRev = document.createElement('i');
+        var revPara = document.createElement('p');
+        testCard.classList.add('card','col','s12','m6', 'l4', 'xl3');
+        testCardImgCon.classList.add('card-image', 'waves-effect', 'waves-block', 'waves-light');
+        testImage.classList.add('activator');
+        testContentCont.classList.add('card-content');
+        testSpan.classList.add('card-title', 'activator','grey-text', 'text-darken-4');
+        testDivreveal.classList.add('card-reveal');
+        testSpanRevTitle.classList.add('card-title','grey-text', 'text-darken-4');
+        closeRev.classList.add('material-icons', 'right');
+        closeRev.textContent = 'close';
+        OpenRev.classList.add('material-icons', 'right');
+        OpenRev.textContent = 'more_vert';
+        testImage.setAttribute('src', 'http://image.tmdb.org/t/p/w342/'+data.results[i].poster_path)+'.jpg';
+        testAnch.setAttribute('data-ytSearch', data.results[0].title);
+        testSpan.textContent = data.results[i].title;
+        testAnch.setAttribute('data-target', 'trailer');
+        // testAnch.setAttribute('href', '#trailer')
+        testAnch.classList.add('waves-effect','btn', 'waves-light', 'modal-trigger', 'findtrailer');
+        testAnch.textContent = 'Watch The Trailer';
+        testSpanRevTitle.textContent = data.results[i].title;
+        revPara.textContent = data.results[i].overview;
+        testCardImgCon.appendChild(testImage);
+        testSpan.appendChild(OpenRev);
+        testContentCont.appendChild(testSpan);
+        testPar.appendChild(testAnch);
+        testContentCont.appendChild(testPar);
+        testSpanRevTitle.appendChild(closeRev);
+        testDivreveal.appendChild(testSpanRevTitle);
+        testDivreveal.appendChild(revPara);
+        testCard.appendChild(testCardImgCon);
+        testCard.appendChild(testContentCont);
+        testCard.appendChild(testDivreveal);
+        pageBody.appendChild(testCard);
+        (function() {
+          testAnch.addEventListener('click', function(e) {
+            findyoutubeid(e.target.dataset.ytsearch)
+          })
+        })(i)
 
         }
         
-        console.log(trailerButton[0].dataset.ytsearch)
+        // console.log(trailerButton[0].dataset.ytsearch)
     })
 }
+// var trailerButton = document.getElementsByClassName('findtrailer')
+// trailerButton.addEventListener('click', findyoutubeid)
+function findyoutubeid(looking){
+  var youtubeAPI = 'AIzaSyDEMdWu3lqGoSduYcL2p7LMJwCINR_eA0o'
+  var youtubeSearchUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='+ looking +'trailer&videoEmbeddable=videoEmbeddableUnspecified&key='+ youtubeAPI;
+  fetch(youtubeSearchUrl)
+      .then(function(response){
+          return response.json();
+  }).then(function(data){
+      console.log(data);
+      trailerVID.setAttribute('src', '')
+      trailerVID.setAttribute('src', 'https://www.youtube.com/embed/'+ data.items[0].id.videoId)
+  })
+  }
 
 // console.log(trailerButton)
 // trailerButton.addEventListener('click', function(event){
