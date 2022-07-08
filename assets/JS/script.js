52969a6570feab9629ee7613697791f345f30428
darmstrong <armstrong2579@gmail.com>
2022-07-08 15:13:38 -0700

    starter object for api's movie id numbers

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -20,7 +20,28 @@ var submitBtn = document.getElementById("submit-btn");
 // New variable to check the radio buttons
 var radioBtns = document.getElementsByName("group1");
 
-
+// Variable to store the number values for each genre for the API
+var genreNumbers = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  crime: 80,
+  documentary: 99,
+  drama: 18,
+  family: 10751,
+  fantasy: 14,
+  history: 36,
+  horror: 27,
+  music: 10402,
+  mystery: 9648,
+  romance: 10749,
+  science fiction: 878,
+  tv movie: 10770,
+  thriller: 53,
+  war: 10752,
+  western: 37
+};
 
 submitBtn.addEventListener('click', checkInput);
 
