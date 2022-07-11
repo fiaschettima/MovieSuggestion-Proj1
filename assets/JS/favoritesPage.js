12faf3bd6da6b59d39b1dfcfc387e8868550b531
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-10 18:13:24 -0700

    tried creating favorites page

--- assets/JS/favoritesPage.js
+++ assets/JS/favoritesPage.js
@@ -1,9 +1,22 @@
 // var favoriteMovies = object.keys(localStorage);
 // console.log(favoriteMovies);
+var movieIds = [];
 
 function forEachKey() {
     for (var i = 0; i < localStorage.length; i++) {
-        console.log(localStorage.getItem(localStorage.key(i)));
-      }
+        movieIds.push(localStorage.getItem(localStorage.key(i)));
+        //console.log(localStorage.getItem(localStorage.key(i)));
+        
+    }
+    // console.log(movieIds);
 }
 forEachKey();
+movieIds.forEach(printFavorites);
+
+function printFavorites (titleId) {
+    console.log(titleId);
+    var tmdbAPI = '07f3bf91adb1325ab2741c977ecdf895';
+    var tmdbURL = 'https://api.themoviedb.org/3/search/movie?api_key=07f3bf91adb1325ab2741c977ecdf895&query=' + titleId+ '&api_key=' + tmdbAPI;
+
+}
+ 
